a454a43f28295016154ed2dded90caa6af3e6dd5
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2024-12-24 23:08:57 +0100

    add GetInstall api

--- internal/workdir/runtime.go
+++ internal/workdir/runtime.go
@@ -34,20 +34,30 @@ type Runtimes struct {
 func NewRuntimes(ctx context.Context, baseDir, specDir string) (*Runtimes, error) {
 	binToolDir := filepath.Join(baseDir, specDir)
 
-	goRuntimes, err := runtimego.Discover(ctx, binToolDir)
-	if err != nil {
-		return nil, fmt.Errorf("discovering go runtimes: %w", err)
-	}
-
-	impls := make(map[string]IRuntime, len(goRuntimes))
-	for _, rt := range goRuntimes {
-		impls[rt.Version()] = rt
-	}
-
-	return &Runtimes{
+	runtimes := &Runtimes{
 		binToolDir: binToolDir,
-		impls:      impls,
-	}, nil
+		impls:      make(map[string]IRuntime),
+	}
+
+	if err := runtimes.discover(ctx); err != nil {
+		return nil, err
+	}
+
+	return runtimes, nil
+}
+
+func (r *Runtimes) discover(ctx context.Context) error {
+	goRuntimes, err := runtimego.Discover(ctx, r.binToolDir)
+	if err != nil {
+		return fmt.Errorf("discovering go runtimes: %w", err)
+	}
+
+	r.impls = make(map[string]IRuntime, len(goRuntimes))
+	for _, rt := range goRuntimes {
+		r.impls[rt.Version()] = rt
+	}
+
+	return nil
 }
 
 func (r *Runtimes) Get(runtime string) (IRuntime, error) {
@@ -57,6 +67,19 @@ func (r *Runtimes) Get(runtime string) (IRuntime, error) {
 	}
 
 	return rt, nil
+}
+
+// GetInstall will get installed runtime or try to install it in other case.
+func (r *Runtimes) GetInstall(ctx context.Context, runtime string) (IRuntime, error) {
+	if rt, err := r.Get(runtime); err == nil {
+		return rt, nil
+	}
+
+	if err := r.Install(ctx, runtime); err != nil {
+		return nil, err
+	}
+
+	return r.Get(runtime)
 }
 
 func (r *Runtimes) Install(ctx context.Context, runtime string) error {
@@ -72,6 +95,10 @@ func (r *Runtimes) Install(ctx context.Context, runtime string) error {
 	ver := strings.TrimPrefix(runtime, runtimeGo+"@")
 	if err := runtimego.Install(ctx, r.binToolDir, ver); err != nil {
 		return fmt.Errorf("install tool runtime (%s): %w", runtime, err)
+	}
+
+	if err := r.discover(ctx); err != nil {
+		return fmt.Errorf("discover tools: %w", err)
 	}
 
 	return nil
