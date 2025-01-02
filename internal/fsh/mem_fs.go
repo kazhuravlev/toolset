a74fc2007c6d099d5b7f8d3139dd164ccb2fea89
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2025-01-02 17:25:46 +0100

    add workdir Init test

--- internal/fsh/mem_fs.go
+++ internal/fsh/mem_fs.go
@@ -1,6 +1,10 @@
 package fsh
 
-import "github.com/spf13/afero"
+import (
+	"fmt"
+	"github.com/spf13/afero"
+	"os"
+)
 
 var _ afero.Linker = (*MemFS)(nil)
 
@@ -10,6 +14,19 @@ type MemFS struct {
 
 func (m *MemFS) SymlinkIfPossible(oldname, newname string) error {
 	return nil
+}
+
+func (m *MemFS) GetTree(dir string) ([]string, error) {
+	res := make([]string, 0)
+	err := afero.Walk(m, dir, func(path string, info os.FileInfo, err error) error {
+		res = append(res, path)
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("walk dir: %w", err)
+	}
+
+	return res, nil
 }
 
 func NewMemFS(files map[string]string) *MemFS {
---
+++ internal/workdir/workdir_test.go
@@ -0,0 +1,25 @@
+package workdir_test
+
+import (
+	"github.com/kazhuravlev/toolset/internal/fsh"
+	"github.com/kazhuravlev/toolset/internal/workdir"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestInit(t *testing.T) {
+	fs := fsh.NewMemFS(nil)
+	const dir = "/dir"
+	require.NoError(t, workdir.Init(fs, dir))
+
+	tree, err := fs.GetTree(dir)
+	require.NoError(t, err)
+	require.Equal(t, []string{
+		"/dir",
+		"/dir/.toolset.json",
+		"/dir/.toolset.lock.json",
+		"/dir/bin",
+		"/dir/bin/tools",
+		"/dir/bin/tools/.stats.json",
+	}, tree)
+}
