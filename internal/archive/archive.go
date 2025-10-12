383fc143212a52a7bf05a0dc77ec71ec93c42b9f
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2025-10-12 16:34:06 +0200

    add archive tools

---
+++ internal/archive/archive.go
@@ -0,0 +1,129 @@
+package archive
+
+import (
+	"archive/tar"
+	"archive/zip"
+	"compress/bzip2"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"path/filepath"
+
+	"github.com/kazhuravlev/toolset/internal/fsh"
+	"github.com/spf13/afero"
+	"github.com/ulikunitz/xz"
+)
+
+func Extract(fs fsh.FS, archivePath, destDir string) error {
+	f, err := fs.Open(archivePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	ext := fsh.Ext(archivePath)
+
+	switch ext {
+	case ".zip":
+		return extractZip(fs, f, destDir)
+	case ".tar.gz", ".tgz":
+		return extractTar(fs, f, destDir, func(r io.Reader) (io.Reader, error) { return gzip.NewReader(r) })
+	case ".tar.bz2":
+		return extractTar(fs, f, destDir, func(r io.Reader) (io.Reader, error) { return bzip2.NewReader(r), nil })
+	case ".tar.xz":
+		return extractTar(fs, f, destDir, func(r io.Reader) (io.Reader, error) { return xz.NewReader(r) })
+	}
+
+	return fmt.Errorf("unsupported archive type (%s)", ext)
+}
+
+func extractZip(fs fsh.FS, f afero.File, dest string) error {
+	info, err := f.Stat()
+	if err != nil {
+		return err
+	}
+
+	r, err := zip.NewReader(f, info.Size())
+	if err != nil {
+		return err
+	}
+
+	for _, zf := range r.File {
+		target := filepath.Join(dest, zf.Name)
+		if zf.FileInfo().IsDir() {
+			if err := fs.MkdirAll(target, 0755); err != nil {
+				return err
+			}
+			continue
+		}
+
+		if err := fs.MkdirAll(filepath.Dir(target), 0755); err != nil {
+			return err
+		}
+
+		rc, err := zf.Open()
+		if err != nil {
+			return err
+		}
+		// FIXME: ...
+		defer rc.Close()
+
+		out, err := fs.Create(target)
+		if err != nil {
+			return err
+		}
+		// FIXME: ...
+		defer out.Close()
+
+		if _, err := io.Copy(out, rc); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func extractTar(fs fsh.FS, f afero.File, dest string, wrap func(io.Reader) (io.Reader, error)) error {
+	reader, err := wrap(f)
+	if err != nil {
+		return err
+	}
+
+	tr := tar.NewReader(reader)
+
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		target := filepath.Join(dest, hdr.Name)
+		switch hdr.Typeflag {
+		case tar.TypeDir:
+			if err := fs.MkdirAll(target, 0755); err != nil {
+				return err
+			}
+		case tar.TypeReg:
+			if err := fs.MkdirAll(filepath.Dir(target), 0755); err != nil {
+				return err
+			}
+
+			out, err := fs.Create(target)
+			if err != nil {
+				return err
+			}
+			// FIXME: ...
+			defer out.Close()
+
+			if _, err := io.Copy(out, tr); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
